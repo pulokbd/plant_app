fbd4d36f4a25e84466944ab9444b04bb31740972
pulokbd <abupulok0@gmail.com>
2022-06-25 02:44:20 +0600

    feat:card widget created for recommended item list

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -13,9 +13,90 @@ class Body extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
+
           HeaderWithSearchBox(size: size),
+
           TitleWithMoreButton(title: "Recomanded", press: (){}),
+
+          RecomandedCard(image: "assets/images/image_1.png", title: "Samantha", country: "Russia",price: 440,press: (){},)
         ],
+      ),
+    );
+  }
+}
+
+class RecomandedCard extends StatelessWidget {
+  RecomandedCard({
+    Key? key, required this.image, required this.title, required this.country, required this.price, required this.press,
+
+
+  }) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding * 2.5,
+        ),
+        width: size.width * 0.4,
+        child: Column(
+          children: [
+            Image.asset(image),
+            Container(
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      offset: const Offset(0, 10),
+                      blurRadius: 50,
+                      color: kPrimaryColor.withOpacity(0.23)
+                  )
+                ]
+              ),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: "$country\n".toUpperCase(),
+                          style: TextStyle(
+                            color: kPrimaryColor.withOpacity(0.5),
+                          )
+                        ),
+                      ]
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    "\$$price",
+                    style: Theme.of(context)
+                    .textTheme
+                    .button?.copyWith(color: kPrimaryColor),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
